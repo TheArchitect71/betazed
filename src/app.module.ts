ae3897ded9cc3ace667bf43fe97f0b7947db4660
Erick Flores <thearchitect@MacBook-Air.local>
2020-11-14 21:00:44 -0600

    new guts

---
+++ src/app.module.ts
@@ -0,0 +1,16 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Connection } from 'typeorm';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
+
+@Module({
+  imports: [TypeOrmModule.forRoot(), AuthModule, UsersModule],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {
+  constructor(private connection: Connection) {}
+}
