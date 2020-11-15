a18c633d545f27aea84df33791bdc26d491a04a7
Erick Flores <thearchitect@MacBook-Air.local>
2020-11-14 22:48:24 -0600

    Welcome JWT part 1

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,16 +1,16 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { Connection } from 'typeorm';
+// import { TypeOrmModule } from '@nestjs/typeorm';
+// import { Connection } from 'typeorm';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(), AuthModule, UsersModule],
+  imports: [AuthModule, UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule {
-  constructor(private connection: Connection) {}
+  // constructor(private connection: Connection) {}
 }
